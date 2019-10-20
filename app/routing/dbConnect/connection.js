214deb8e6d22af20748eab5aaf2549be60148f3f
Ryan Mobley <ryan.mobley94@gmail.com>
2019-10-20 17:55:31 -0500

    forgot how heroku works

--- app/routing/dbConnect/connection.js
+++ app/routing/dbConnect/connection.js
@@ -1,12 +1,17 @@
 const sql = require('mysql');
 require('dotenv').config();
-connection = sql.createConnection({
-    host     : process.env.SQL_HOST.toString(),
-    user     : process.env.SQL_USER.toString(),
-    port     : 3306,
-    password : process.env.SQL_PW.toString(),
-    database : process.env.DB_NAME.toString(),
-});
+if (process.env.JAWSDB_URL) {
+    connection = sql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = sql.createConnection({
+        host     : process.env.SQL_HOST.toString(),
+        user     : process.env.SQL_USER.toString(),
+        port     : 3306,
+        password : process.env.SQL_PW.toString(),
+        database : process.env.DB_NAME.toString(),
+    });
+}
+
 
 connection.connect((err)=> {
     if (err) throw err;
