e51cc59c604a5db5539b8fe52bb4da5ea08bde3c
Ryan Mobley <ryan.mobley94@gmail.com>
2019-10-20 17:01:34 -0500

    preparing to deploy to heroku.......

--- app/routing/dbConnect/connection.js
+++ app/routing/dbConnect/connection.js
@@ -5,7 +5,7 @@ connection = sql.createConnection({
     user     : 'root',
     port     : 3306,
     password : process.env.SQL_PW.toString(),
-    database : 'classact',
+    database : process.env.DB_NAME.toString(),
 });
 
 connection.connect((err)=> {
